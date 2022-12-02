d61e330d545082405203d33ee94bd29f361aa9ad
Gabriel Cerutti <gabriel.cerutti@accuv.com>
2022-12-02 10:44:45 +0100

    Updated nuspec dependencies

---
+++ Example.NetCore/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var logger = log4net.LogManager.GetLogger("Program");
+
+var fileInfo = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config");
+if (fileInfo.Exists)
+    log4net.Config.XmlConfigurator.Configure(fileInfo);
+else
+    throw new InvalidOperationException("No log config file found");
+
+int count = 0;
+
+while (true)
+{
+    Thread.Sleep(2000);
+    logger.Debug($"Log number {count++}");
+    logger.Info($"Log number {count++}");
+    logger.Warn($"Log number {count++}");
+    logger.Error($"Log number {count++}");
+    logger.Fatal($"Log number {count++}");
+}
